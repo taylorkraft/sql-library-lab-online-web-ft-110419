15b8d5198dac476dfde750e70bdcc3ce14a8b63e
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-25 00:45:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+   FROM series
+   INNER JOIN authors
+   ON series.author_id = author.id
+   INNER JOIN subgenres
+   ON series.subgenre_id = subgenre.id;"
 end
 
 def select_series_title_with_most_human_characters
