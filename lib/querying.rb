e6a7b507627caa2810f9d89637dc70bb03f4e1ed
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-25 00:47:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,9 +32,16 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title, COUNT(*)
+  FROM books
+  JOIN character_books"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*) AS book_count
+   FROM character_books
+   JOIN characters
+   ON character_books.character_id - character.id
+   GROUP BY characters.name
+   ORDER BY book_count DESC"
 end
